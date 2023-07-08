b53b47425134468368de4643d4b89cb23e44419a
Felix Rindt <felix@rindt.me>
2023-07-08 11:39:55 +0200

    expand aggregation test

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -8,4 +8,8 @@ def test_aggregator():
     source_folder = data_folder
     output_folder = data_folder / "output"
     aggregator = Aggregator(source_folder, output_folder, dry_run=True, seperator_frames=30)
+    input_matches = list(aggregator.search_for_input_matches())
+    assert len(input_matches) == 4
+    aggregations = list(aggregator.get_aggregations(input_matches))
+    assert len(aggregations) == 2
     aggregator.run()
